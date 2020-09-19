c43cec41a1de9af628fe521c8fa1fbcdabaf88a9
Huanhuan0918 <hxp55@mail.missouri.edu>
2020-09-18 23:33:33 -0500

    Pop up test. Component load failed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, VERSION } from "@angular/core";
+import { Component, Injector } from "@angular/core";
+import { createCustomElement } from '@angular/elements';
+import { PopupService } from './popup.service';
+import { PopupComponent } from './popup/popup.component';
 
 @Component({
   selector: "my-app",
@@ -7,5 +10,10 @@ import { Component, VERSION } from "@angular/core";
 })
 
 export class AppComponent {
-  title = 'My Habits';
+    constructor(injector: Injector, public popup: PopupService) {
+    // Convert `PopupComponent` to a custom element.
+    const PopupElement = createCustomElement(PopupComponent, {injector});
+    // Register the custom element with the browser.
+    customElements.define('popup-element', PopupElement);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -9,19 +10,24 @@ import { HabitDetailComponent } from './habit-detail/habit-detail.component'
 
 import { AppRoutingModule } from './app-routing.module';
 import { HabitCreateComponent } from './habit-create/habit-create.component';
+import { PopupComponent } from './popup/popup.component';
+import { PopupService } from './popup.service';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
+  providers: [PopupService],
   declarations: [ 
     AppComponent, 
     TopBarComponent,
     HabitsComponent,
     HabitDetailComponent,
-    HabitCreateComponent
+    HabitCreateComponent,
+    PopupComponent
   ],
   bootstrap:    [ AppComponent ]
 })
