eea8a121545c950df29512df2b77feb97ecea94e
Huanhuan0918 <hxp55@mail.missouri.edu>
2020-09-18 21:23:54 -0500

    Improve format.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
   { path: '', component: HabitsComponent, pathMatch: 'full' },
   { path: 'habit-create', component: HabitCreateComponent },
   { path: 'detail/:name', component: HabitDetailComponent }
-
 ];
 
 @NgModule({
