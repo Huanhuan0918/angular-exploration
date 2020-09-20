9e29f79ce232196b5b43e820043250500566d9af
hxp55 <hxp55@mail.missouri.edu>
2020-09-19 20:11:35 -0500

    Merging my angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, VERSION } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './animation';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: "my-app",
@@ -11,6 +12,10 @@ import { slideInAnimation } from './animation';
 
 export class AppComponent {
   title = 'My Habits';
+  
+  constructor() {
+    console.log(environment.production); // Logs false for default environment
+  }
 
   // The AppComponent method that detects when a route change happens.
   // Takes the value of outlet, and returns the state of the animation
